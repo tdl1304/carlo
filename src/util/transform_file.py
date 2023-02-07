bdc4d3ad03bcc1f6c1fcef3467c8cf39f4fb13de
Ole August <oleastole@gmail.com>
2023-02-07 15:45:11 +0100

    fix: support PosixPath

--- src/util/transform_file.py
+++ src/util/transform_file.py
@@ -21,12 +21,12 @@ class TransformFile:
             self.output_dir = root_path / str(int(datetime.timestamp(dt)))
         
         self.output_dir.mkdir(exist_ok=True, parents=True)
-        self.image_dir = self.output_dir + '/images'
+        self.image_dir = self.output_dir / 'images'
         self.image_dir.mkdir(exist_ok=True, parents=True)
 
     def append_frame(self, image: ndarray, transform: carla.Transform):
         # Save the image to output
-        file_path = self.image_dir + '/' + f'{self.count:04d}' + '.png'
+        file_path = str(self.image_dir / f'{self.count:04d}.png')
         cv2.imwrite(file_path, image)
         self.count += 1
 
@@ -42,7 +42,7 @@ class TransformFile:
         self.intrinsics = intrinsics
 
     def export_transforms(self, file_path='transforms.json'):
-        output_path = self.output_dir + '/' + file_path
+        output_path = self.output_dir / file_path
         with open(output_path, 'w+') as f:
             obj = {
                 'intrinsics': self.intrinsics,
