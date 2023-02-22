ba833e1afdd8846a0720c9ca57e021d3a946298b
Ole August <oleastole@gmail.com>
2023-02-22 15:50:03 +0100

    feat: archive old transforms for reference

--- src/util/convert_transforms.py
+++ src/util/convert_transforms.py
@@ -5,8 +5,8 @@ from pathlib import Path
 import numpy as np
 
 def conversion(matrix: np.ndarray):
-    conversion_matrix = np.array([[1, 0, 0, 0],
-                        [0, 0, -1, 0],
+    conversion_matrix = np.array([[5, 0, 0, 0],
+                        [0, 0, -2, 0],
                         [0, 1, 0, 0],
                         [0, 0, 0, 1]])
     new_matrix = np.matmul(conversion_matrix, matrix)
@@ -15,16 +15,19 @@ def conversion(matrix: np.ndarray):
 def convert():
     root_path = Path(os.curdir)
     run_path = root_path / 'runs/no_conversion_run'
-    transforms_path = run_path / 'transforms.json'
+    transforms_path = run_path / 'original_transforms.json'
+    
+    # Variables for 
     frames = None
+    new_transforms = None
+    new_frames = []
 
     with open(transforms_path, 'r') as f:
-        transforms = json.load(f)
-        frames = transforms['frames']
+        new_transforms = json.load(f)
+        frames = new_transforms['frames']
 
-    new_frames = []
+    # Convert each frame with the conversion matrix
     for frame in frames:
-        
         new_transform = conversion(frame['transform_matrix'])
         new_frame = {
             'file_path': frame['file_path'],
@@ -32,17 +35,22 @@ def convert():
         }
         new_frames.append(new_frame)
     
+    # Overwrite frames
+    new_transforms['frames'] = new_frames
 
-    # Create new file-path
+    # Create archive file-path
     dt = datetime.now()
-    new_file_path = run_path / f'{int(datetime.timestamp(dt))}_transforms.json'
-
-    # Save new transforms
-    with open(new_file_path, 'w+') as f:
-        transforms['frames'] = new_frames
-        json.dump(transforms, f, indent=4)
-
-    print(f"\nNew transforms saved to {new_file_path}")
+    archive_path = run_path / "archive" / f'{int(datetime.timestamp(dt))}_transforms.json'
+    
+    # Save new transforms to archive
+    with open(archive_path, 'w+') as f:
+        json.dump(new_transforms, f, indent=4)
+    
+    # Overwrite old transforms
+    with open(run_path / "transforms.json", 'w+') as f:
+        json.dump(new_transforms, f, indent=4)
+        print(f"\nSuccessfully overwrote transforms.json")
+    print(f"\nNew transforms saved to {archive_path}")
 
 if __name__ == "__main__":
     convert()
