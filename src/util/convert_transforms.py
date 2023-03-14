f94d626231a40b4c8f1ae76e4ff0397dcd3f540b
Ole August Støle <oleastole@gmail.com>
2023-03-14 13:49:20 +0100

    feat: create conversion-script for converting transforms.json-files

--- src/util/convert_transforms.py
+++ src/util/convert_transforms.py
@@ -5,39 +5,76 @@ import sys
 from pathlib import Path
 import numpy as np
 
+from scipy.spatial.transform import Rotation
+
+
+def rotate_matrix(input_matrix, axis, degrees):
+    # convert degrees to radians
+    theta = np.radians(degrees)
+    # create a rotation matrix depending on the axis
+    if axis == 'x':
+        R = [[1, 0, 0, 0],
+            [0, np.cos(theta), -np.sin(theta), 0],
+            [0, np.sin(theta), np.cos(theta), 0],
+            [0, 0, 0, 1]]
+    elif axis == 'y':
+        R = [[np.cos(theta), 0, np.sin(theta), 0],
+            [0, 1, 0, 0],
+            [-np.sin(theta), 0, np.cos(theta), 0],
+            [0, 0, 0, 1]]
+    elif axis == 'z':
+        R = [[np.cos(theta), -np.sin(theta), 0, 0],
+            [np.sin(theta), np.cos(theta), 0, 0],
+            [0, 0, 1, 0],
+            [0, 0, 0, 1]]
+    else:
+        print("Invalid axis. Please choose 'x', 'y', or 'z'.")
+        return
+    # multiply the rotation matrix with the input matrix
+    output_matrix = np.dot(R, input_matrix)
+    
+    # return the rotated matrix
+    return output_matrix
+
 def conversion(matrix: np.ndarray):
-    matrix_1 = np.array([[1, 0, 0, 0],
-                [0, 0, -1, 0],
-                [0, 1, 0, 0],
-                [0, 0, 0, 1]])
+    matrix = np.array(matrix)
 
-    # # Step 1: Flip around Z
-    # matrix_2 = np.array([[0, 0, 1, 0],
-    #                 [0, 1, 0, 0],
-    #                 [-1, 0, 0, 0],
-    #                 [0, 0, 0, 1]])
+    # matrix = rotate_matrix(matrix, 'y', 90)
 
-    # # Step 1: Flip around Y
-    # matrix_3 = np.array([[0, -1, 0, 0],
-    #                         [1, 0, 0, 0],
-    #                         [0, 0, 1, 0],
-    #                         [0, 0, 0, 1]])
-    
-    # Step 2: Multiply the transformation matrix with the conversion matrix
-    transformed_matrix = np.matmul(matrix_1, matrix)
-    # transformed_matrix = np.matmul(matrix_2, transformed_matrix)
-    # transformed_matrix = np.matmul(matrix_3, transformed_matrix)
-    
-    # # Step 3: Flip the y and z coordinates of the resulting matrix
-    # transformed_matrix[0] = -transformed_matrix[0]
-    # transformed_matrix[1] = -transformed_matrix[1]
-    
+    # Create a rotation matrix around the local X-axis
+    rotation_matrix = Rotation.from_euler("Z", [270], degrees=True).as_matrix()
+
+    # Extract the 3x3 submatrix from the object matrix
+    orientation_matrix = matrix[:3, :3]
+
+    # Multiply the rotation matrix with the orientation matrix
+    new_orientation_matrix = rotation_matrix @ orientation_matrix
+
+    # Replace the 3x3 submatrix in the object matrix with the new orientation matrix
+    matrix[:3, :3] = new_orientation_matrix
+
+    matrix = rotate_matrix(matrix, 'y', 90)
+ 
+
+    # Create a rotation matrix around the local X-axis
+    rotation_matrix = Rotation.from_euler("xz", [90, 90], degrees=True).as_matrix()
+
+    # Extract the 3x3 submatrix from the object matrix
+    orientation_matrix = matrix[:3, :3]
+
+    # Multiply the rotation matrix with the orientation matrix
+    new_orientation_matrix = rotation_matrix @ orientation_matrix
+
+    # Replace the 3x3 submatrix in the object matrix with the new orientation matrix
+    matrix[:3, :3] = new_orientation_matrix
+
+
     # Convert the numpy array to a list of lists and return the resulting matrix
-    return transformed_matrix.tolist()
+    return matrix.tolist()
 
 def convert(file_prefix: str = None):
     root_path = Path(os.curdir)
-    run_path = root_path / 'runs/no_conversion_run'
+    run_path = root_path / 'runs/conversion_run'
     transforms_path = run_path / 'original_transforms.json'
     
     # Variables for 
@@ -49,9 +86,14 @@ def convert(file_prefix: str = None):
         new_transforms = json.load(f)
         frames = new_transforms['frames']
 
+    num_convert = len(frames) / 1
     # Convert each frame with the conversion matrix
-    for frame in frames:
-        new_transform = conversion(frame['transform_matrix'])
+    for i, frame in enumerate(frames):
+        if i < num_convert:
+            new_transform = conversion(frame['transform_matrix'])
+        else:
+            new_transform = frame['transform_matrix']
+        
         new_frame = {
             'file_path': frame['file_path'],
             'transform_matrix': new_transform
